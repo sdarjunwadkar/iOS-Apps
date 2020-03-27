dfdf6d01630a27d7f3c4bb9c494958349f94d33a
Sharvil Deepak Arjunwadkar <59260370+sdarjunwadkar@users.noreply.github.com>
2020-03-27 22:07:21 +0530

    Add files via upload

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Sharvil Arjunwadkar on 14/03/20.
+//  Copyright © 2020 Sharvil Arjunwadkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
